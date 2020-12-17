fbc3daa63f45fcb81143c6e3fe329334ef68e5b4
Jeremy A Gray <jeremy.a.gray@gmail.com>
2020-12-17 12:48:22 -0600

    feat: add socket.io to server
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 // const mongoose = require('mongoose');
 const nocache = require('nocache');
-const socket = require('socket.io');
+const io = require('socket.io');
 
 // Middleware.
 const helmet = require('./middleware/helmet.js');
@@ -24,12 +24,12 @@ const runner = require('./test-runner');
 const app = express();
 
 async function start() {
-//   // Configure mongoose.
-//   const MONGOOSE_OPTIONS = {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false
-//   };
+  // Configure mongoose.
+  // const MONGOOSE_OPTIONS = {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  //   useFindAndModify: false
+  // };
 
   try {
 //     await mongoose.connect(process.env.MONGO_URI, MONGOOSE_OPTIONS);
@@ -43,21 +43,21 @@ async function start() {
       next();
     });
     
-//     // FCC testing.
-//     app.use(cors({origin: '*'}));
+    // FCC testing.
+    // app.use(cors({origin: '*'}));
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
-//     app.set('trust proxy', true);
+    // app.set('trust proxy', true);
 
     // Set static directory.
     app.use('/public', express.static(process.cwd() + '/public'));
     app.use('/assets', express.static(process.cwd() + '/assets'));
 
-//     // Set view directory and view engine.
-//     app.set('views', process.cwd() + '/views');
-//     app.set('view engine', 'pug');
+    // Set view directory and view engine.
+    app.set('views', process.cwd() + '/views');
+    app.set('view engine', 'pug');
 
     // Serve static index.
     app.route('/')
@@ -84,7 +84,7 @@ async function start() {
     const name = 'fcc-isp-srtm-game';
     const version = '0.0.1';
 
-    app.listen(port, function () {
+    const server = app.listen(port, await function () {
       console.log(`${name}@${version} listening on port ${port}...`);
       if (process.env.NODE_ENV ==='test') {
         console.log(`${name}@${version} running unit and functional tests...`);
@@ -98,6 +98,9 @@ async function start() {
         }, 1500);
       }
     });
+
+    // Connect socket.
+    io.listen(server);
 
     // Export app for testing.
     module.exports = app;
